5f342c31beb18e549073941351dd0489c499cfaf
vagrant <vagrant@homestead>
2019-04-07 22:01:48 +0800

    add unit test

--- tests/MD5hashTest.php
+++ tests/MD5hashTest.php
@@ -20,4 +20,29 @@ class MD5hashTest extends TestCase
 
         $this->assertEquals($this->hasher->make($password), md5($password));
     }
+
+    public function testMakeWithSalt()
+    {
+        $password = 'maxfine';
+        $salt = '%^&';
+
+        $this->assertEquals($this->hasher->make($password, ['salt' => $salt]), md5($password.$salt));
+    }
+
+    public function testCheck()
+    {
+        $password = 'maxfine';
+        $passwordTwo = md5($password);
+
+        $this->assertTrue($this->hasher->check($password, $passwordTwo));
+    }
+
+    public function testCheckWithSalt()
+    {
+        $password = 'maxfine';
+        $salt = '%^&';
+        $passwordTwo = md5($password.$salt);
+
+        $this->assertTrue($this->hasher->check($password, $passwordTwo, ['salt' => $salt]));
+    }
 }
